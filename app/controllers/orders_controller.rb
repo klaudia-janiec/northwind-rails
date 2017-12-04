f055b4c28bacfcfac533a3100e5115cbb73d5877
klaudia <klaudia.janiec@gmail.com>
2017-12-04 20:30:10 +0100

    Add delete action to orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,22 @@
 class OrdersController < ApplicationController
+  before_action :order, only: %i[show destroy]
   def index
     @orders = Order.includes(:customer, :employee, :shipper, order_details: :product).order(:id).all
   end
 
-  def show
+  def show; end
+
+  def destroy
+    if order.destroy
+      redirect_to root_url
+    else
+      flash[:error] = order.errors.full_messages
+    end
+  end
+
+  private
+
+  def order
     @order = Order.find(params[:id])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  has_many :order_details
+  has_many :order_details, dependent: :destroy
   has_many :products, through: :order_details
   belongs_to :customer, optional: true
   belongs_to :employee, optional: true
