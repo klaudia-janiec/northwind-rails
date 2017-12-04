d72c8bb033135aba97c9ae61d48ed9276692470a
klaudia <klaudia.janiec@gmail.com>
2017-12-04 19:46:46 +0100

    Add show order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,8 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.includes(:customer, :employee, :shipper, order_details: :product).order(:id).all
   end
+
+  def show
+    @order = Order.find(params[:id])
+  end
 end
