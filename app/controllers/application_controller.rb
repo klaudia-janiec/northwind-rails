a0c95ca5cb2d89ebdf355521a96fae7286f06042
klaudia <klaudia.janiec@gmail.com>
2017-12-05 23:53:43 +0100

    Error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_error
+
+  def rescue_from_error(exception)
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "orders#index"
   resources :orders
+  match "*path", to: "orders#index", via: :all
 end
